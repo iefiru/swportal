f054ac55063fdad6d19d02ac06de931932d8f4e9
iefiru <7875793+iefiru@users.noreply.github.com>
2024-06-01 13:45:41 +0800

    Update do.py

--- do.py
+++ do.py
@@ -1,9 +1,21 @@
+import click
 from workflowscript.meetupGenerator import ParsingModel
 
-if __name__ == '__main__':
+SESH_TOKEN=""
+@click.command()
+@click.option('-t', '--token', 'SESH token', help='SESH Token')
+def meetup_handle(token):
+    SESH_TOKEN = token
+    print(f'SESH_TOKEN, { SESH_TOKEN or "stranger"}!')
+    
+    
     # get the template file path
     fsm_template_path = './workflowscript/meetup.template'
 
     # test on rst file
     parsingModel = ParsingModel(fsm_template_path)
     parsingModel()
+
+
+if __name__ == '__main__':
+    meetup_handle()
